19f43d807be42ec7b28f316c292d4623b5bcfa86
Tim Weller <tww902@jagmail.southalabama.edu>
2012-11-03 14:05:15 -0500

    Separating project code into NXC and LEJOS and adding first version of LEJOS code.

---
+++ lejos/SensorStatus.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.robotics.RegulatedMotor;
+import lejos.util.Delay;
+import lejos.nxt.*;
+public class SensorStatus implements Runnable{
+	public void run(){
+		while(true){
+			PapaMain.lightFrontValue = PapaMain.lightFront.readValue();
+			LCD.drawInt(PapaMain.lightFrontValue,0,0);
+			if(PapaMain.lightFrontValue >= PapaMain.lightFrontThreshold){
+				PapaMain.lightFrontDetect = true;
+			}
+			else {
+				PapaMain.lightFrontDetect = false;
+			}
+			PapaMain.lightRearValue = PapaMain.lightRear.readValue();
+			LCD.drawInt(PapaMain.lightRearValue,0,1);
+			if(PapaMain.lightRearValue >= PapaMain.lightRearThreshold){
+				PapaMain.lightRearDetect = true;
+			}
+			else {
+				PapaMain.lightRearDetect = false;
+			}
+			if(PapaMain.leftTouch.isPressed()){
+				LCD.drawString("Left touch", 0, 2);
+				PapaMain.leftTouched = true;
+			}
+			else {
+				LCD.clear(2);
+				PapaMain.leftTouched = false;
+			}
+			if(PapaMain.rightTouch.isPressed()){
+				LCD.drawString("Right touch", 0, 3);
+				PapaMain.rightTouched = true;
+			}
+			else {
+				LCD.clear(3);
+				PapaMain.rightTouched = false;
+			}
+		}
+	}
+}
